f7db5b3282a174e0b4ca95b95dbe0869945cc1b1
mike Tarica <mike.tarica@me.com>
2022-07-03 14:03:33 +0200

    earch function

--- js/funtionalaty.js
+++ js/funtionalaty.js
@@ -1,12 +1,31 @@
 // pop up box
-window.alert("sometext");
-console.log(message)
+// window.alert("sometext");
+// console.log(message)
 
-let person = prompt("Please enter your name", "Harry Potter");
+let person = prompt("Please enter your name so we can make it a personal experince for you", "your name");
 let text;
 if (person == null || person == "") {
     text = "User cancelled the prompt.";
 } else {
     text = "Hello " + person + "! How are you today?";
 }
-console.log(message)
+Console.log(person);
+// Pop up disclaimor box
+// $(function () {
+//             var overlay = $('<div id="overlay"></div>');
+//             overlay.show();
+//             overlay.appendTo(document.body);
+//             $('.popup').show();
+//             $('.close').click(function () {
+//                 $('.popup').hide();
+//                 overlay.appendTo(document.body).remove();
+//                 return false;
+//             });
+
+
+//     $('.x').click(function () {
+//     $('.popup').hide();
+//     overlay.appendTo(document.body).remove();
+//     return false;
+//     });
+//     });
